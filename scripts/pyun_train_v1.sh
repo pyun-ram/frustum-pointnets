b0c5595c2ad3827347da9abd93ecf23b6e66de95
pyun <pyun@connect.ust.hk>
2019-02-21 15:20:22 +0800

    19022100

--- scripts/pyun_train_v1.sh
+++ scripts/pyun_train_v1.sh
@@ -1,2 +1,3 @@
 #/bin/bash
-python train/train.py --gpu 0 --model frustum_pointnets_v1 --log_dir train/log_pyun_debug3 --num_point 1024 --max_epoch 201 --batch_size 32 --decay_step 800000 --decay_rate 0.5
+TAG=19022100
+python train/train.py --gpu 0 --model frustum_pointnets_v1 --log_dir train/log_$TAG --num_point 1024 --max_epoch 201 --batch_size 32 --decay_step 800000 --decay_rate 0.5
